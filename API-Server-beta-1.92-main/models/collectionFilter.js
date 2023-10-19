182ea3352fccbad708d17719d8281b9c5f2115ba
elisalac <elisa.lacombe@icloud.com>
2023-10-19 17:27:35 -0400

    Update collectionFilter.js

--- API-Server-beta-1.92-main/models/collectionFilter.js
+++ API-Server-beta-1.92-main/models/collectionFilter.js
@@ -7,7 +7,41 @@ export default class CollectionFilter extends Model {
         this.params = params;
     }
 
-    get() {
+    static Filter() {
 
     }
+
+    static Sort() {
+
+    }
+
+    static LimitOffset() {
+
+    }
+
+    static Fields() {
+
+    }
+}
+
+function valueMatch(value, searchValue) {
+    try {
+        let exp = '^' + searchValue.toLowerCase().replace(/\*/g, '.*') + '$';
+        return new RegExp(exp).test(value.toString().toLowerCase());
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+function compareNum(x, y) {
+    if (x === y) return 0;
+    else if (x < y) return -1;
+    return 1;
+}
+
+function innerCompare(x, y) {
+    if ((typeof x) === 'string')
+        return x.localeCompare(y);
+    else
+        return this.compareNum(x, y);
 }
